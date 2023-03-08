eb5f6b19bcfab4c0838b13d3ed550f8710bb9e79
Shamukh <gsssirivadan1111@gmail.com>
2023-03-08 11:28:29 -0500

    DBScript

---
+++ StyleSphere/DBScript/Script03-08-23.sql
@@ -0,0 +1,353 @@
+USE [master]
+GO
+/****** Object:  Database [StyleSphereDB]    Script Date: 3/8/2023 11:22:20 AM ******/
+CREATE DATABASE [StyleSphereDB]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'StyleSphereDB', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\StyleSphereDB.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'StyleSphereDB_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\StyleSphereDB_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [StyleSphereDB] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [StyleSphereDB].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [StyleSphereDB] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [StyleSphereDB] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [StyleSphereDB] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [StyleSphereDB] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [StyleSphereDB] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET RECOVERY FULL 
+GO
+ALTER DATABASE [StyleSphereDB] SET  MULTI_USER 
+GO
+ALTER DATABASE [StyleSphereDB] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [StyleSphereDB] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [StyleSphereDB] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [StyleSphereDB] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [StyleSphereDB] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [StyleSphereDB] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'StyleSphereDB', N'ON'
+GO
+ALTER DATABASE [StyleSphereDB] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [StyleSphereDB] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [StyleSphereDB]
+GO
+/****** Object:  Table [dbo].[Categories]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categories](
+	[CategoryID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [varchar](150) NOT NULL,
+	[Description] [varchar](max) NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+	[ShowOnTop] [bit] NOT NULL,
+ CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
+(
+	[CategoryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ColorMaster]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ColorMaster](
+	[ColorID] [int] IDENTITY(1,1) NOT NULL,
+	[Color] [nvarchar](50) NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+ CONSTRAINT [PK_ColorMaster] PRIMARY KEY CLUSTERED 
+(
+	[ColorID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Customers]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customers](
+	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerName] [varchar](50) NOT NULL,
+	[Email] [varchar](50) NOT NULL,
+	[Password] [varchar](50) NOT NULL,
+	[ContactNo] [varchar](50) NOT NULL,
+	[Address] [varchar](150) NOT NULL,
+	[ZipCode] [varchar](50) NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Favorites]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Favorites](
+	[FavoritesID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerID] [int] NOT NULL,
+	[ProductID] [int] NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+ CONSTRAINT [PK_Favorites] PRIMARY KEY CLUSTERED 
+(
+	[FavoritesID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[OrderDetails]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OrderDetails](
+	[OrderDetailsID] [int] IDENTITY(1,1) NOT NULL,
+	[Quantity] [nvarchar](50) NOT NULL,
+	[Price] [numeric](10, 2) NOT NULL,
+	[ProductMappingID] [int] NOT NULL,
+	[OrderID] [int] NOT NULL,
+	[Total] [nvarchar](50) NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+ CONSTRAINT [PK_OrderDetails] PRIMARY KEY CLUSTERED 
+(
+	[OrderDetailsID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[OrdersData]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OrdersData](
+	[OrderID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerID] [int] NOT NULL,
+	[OrderDate] [date] NOT NULL,
+	[ShippingAddress] [varchar](250) NOT NULL,
+	[BillingAddress] [varchar](250) NOT NULL,
+	[TrackingID] [varchar](50) NOT NULL,
+	[NetAmount] [numeric](10, 2) NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+ CONSTRAINT [PK_OrdersData] PRIMARY KEY CLUSTERED 
+(
+	[OrderID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ProductMappings]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ProductMappings](
+	[ProductMappingID] [int] IDENTITY(1,1) NOT NULL,
+	[ProductID] [int] NOT NULL,
+	[SizeID] [int] NOT NULL,
+	[ColorID] [int] NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+ CONSTRAINT [PK_ProductMappings] PRIMARY KEY CLUSTERED 
+(
+	[ProductMappingID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Products]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[ProductID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryID] [int] NOT NULL,
+	[SubCategoryID] [int] NOT NULL,
+	[ProductName] [varchar](150) NOT NULL,
+	[Price] [numeric](10, 2) NOT NULL,
+	[MfgDate] [date] NOT NULL,
+	[ThumbnailImage] [varchar](max) NOT NULL,
+	[Image1] [varchar](max) NOT NULL,
+	[Image2] [varchar](max) NULL,
+	[Image3] [varchar](max) NULL,
+	[Description] [varchar](max) NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Ratings]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Ratings](
+	[RatingID] [int] IDENTITY(1,1) NOT NULL,
+	[ProductID] [int] NOT NULL,
+	[CustomerID] [int] NOT NULL,
+	[Rating] [int] NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+ CONSTRAINT [PK_Ratings] PRIMARY KEY CLUSTERED 
+(
+	[RatingID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[SizesMaster]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SizesMaster](
+	[SizeID] [int] IDENTITY(1,1) NOT NULL,
+	[EUSize] [nvarchar](50) NOT NULL,
+	[USSize] [nvarchar](50) NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+ CONSTRAINT [PK_SizesMaster] PRIMARY KEY CLUSTERED 
+(
+	[SizeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Sub_Categories]    Script Date: 3/8/2023 11:22:21 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Sub_Categories](
+	[SubCategoryID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryID] [int] NOT NULL,
+	[SubCategoryName] [nvarchar](50) NOT NULL,
+	[Description] [nvarchar](50) NOT NULL,
+	[ActiveStatus] [bit] NOT NULL,
+ CONSTRAINT [PK_Sub Categories] PRIMARY KEY CLUSTERED 
+(
+	[SubCategoryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Favorites]  WITH CHECK ADD  CONSTRAINT [FK_Favorites_Customers] FOREIGN KEY([CustomerID])
+REFERENCES [dbo].[Customers] ([CustomerID])
+GO
+ALTER TABLE [dbo].[Favorites] CHECK CONSTRAINT [FK_Favorites_Customers]
+GO
+ALTER TABLE [dbo].[Favorites]  WITH CHECK ADD  CONSTRAINT [FK_Favorites_Products] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Products] ([ProductID])
+GO
+ALTER TABLE [dbo].[Favorites] CHECK CONSTRAINT [FK_Favorites_Products]
+GO
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_OrdersData] FOREIGN KEY([OrderID])
+REFERENCES [dbo].[OrdersData] ([OrderID])
+GO
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_OrdersData]
+GO
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_ProductMappings] FOREIGN KEY([ProductMappingID])
+REFERENCES [dbo].[ProductMappings] ([ProductMappingID])
+GO
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_ProductMappings]
+GO
+ALTER TABLE [dbo].[OrdersData]  WITH CHECK ADD  CONSTRAINT [FK_OrdersData_Customers] FOREIGN KEY([CustomerID])
+REFERENCES [dbo].[Customers] ([CustomerID])
+GO
+ALTER TABLE [dbo].[OrdersData] CHECK CONSTRAINT [FK_OrdersData_Customers]
+GO
+ALTER TABLE [dbo].[ProductMappings]  WITH CHECK ADD  CONSTRAINT [FK_ProductMappings_ColorMaster] FOREIGN KEY([ColorID])
+REFERENCES [dbo].[ColorMaster] ([ColorID])
+GO
+ALTER TABLE [dbo].[ProductMappings] CHECK CONSTRAINT [FK_ProductMappings_ColorMaster]
+GO
+ALTER TABLE [dbo].[ProductMappings]  WITH CHECK ADD  CONSTRAINT [FK_ProductMappings_Products] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Products] ([ProductID])
+GO
+ALTER TABLE [dbo].[ProductMappings] CHECK CONSTRAINT [FK_ProductMappings_Products]
+GO
+ALTER TABLE [dbo].[ProductMappings]  WITH CHECK ADD  CONSTRAINT [FK_ProductMappings_SizesMaster] FOREIGN KEY([SizeID])
+REFERENCES [dbo].[SizesMaster] ([SizeID])
+GO
+ALTER TABLE [dbo].[ProductMappings] CHECK CONSTRAINT [FK_ProductMappings_SizesMaster]
+GO
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Products_Categories] FOREIGN KEY([CategoryID])
+REFERENCES [dbo].[Categories] ([CategoryID])
+GO
+ALTER TABLE [dbo].[Products] CHECK CONSTRAINT [FK_Products_Categories]
+GO
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Products_Sub_Categories] FOREIGN KEY([SubCategoryID])
+REFERENCES [dbo].[Sub_Categories] ([SubCategoryID])
+GO
+ALTER TABLE [dbo].[Products] CHECK CONSTRAINT [FK_Products_Sub_Categories]
+GO
+ALTER TABLE [dbo].[Ratings]  WITH CHECK ADD  CONSTRAINT [FK_Ratings_Customers] FOREIGN KEY([CustomerID])
+REFERENCES [dbo].[Customers] ([CustomerID])
+GO
+ALTER TABLE [dbo].[Ratings] CHECK CONSTRAINT [FK_Ratings_Customers]
+GO
+ALTER TABLE [dbo].[Ratings]  WITH CHECK ADD  CONSTRAINT [FK_Ratings_Products] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Products] ([ProductID])
+GO
+ALTER TABLE [dbo].[Ratings] CHECK CONSTRAINT [FK_Ratings_Products]
+GO
+ALTER TABLE [dbo].[Sub_Categories]  WITH CHECK ADD  CONSTRAINT [FK_Sub Categories_Categories] FOREIGN KEY([SubCategoryID])
+REFERENCES [dbo].[Categories] ([CategoryID])
+GO
+ALTER TABLE [dbo].[Sub_Categories] CHECK CONSTRAINT [FK_Sub Categories_Categories]
+GO
+USE [master]
+GO
+ALTER DATABASE [StyleSphereDB] SET  READ_WRITE 
+GO
